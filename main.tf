0e9bff4b553e2d0d8ab88b58e1a85da42fd1f740
Kleiton Marques <kleitonm.marques@gmail.com>
2023-01-28 18:13:28 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_droplet" "web" {
+  image    = "ubuntu-22-04-x64"
+  name     = "jenkins"
+  region   = var.region
+  size     = "s-2vcpu-2gb"
+  ssh_keys = [data.digitalocean_ssh_key.digitalocean.id]
+}
+
+data "digitalocean_ssh_key" "digitalocean" {
+  name = "digitalocean"
+}
+
+resource "digitalocean_kubernetes_cluster" "k8s" {
+  name   = "k8s"
+  region = var.region
+  # Grab the latest version slug from `doctl kubernetes options versions`
+  version = "24.4.8-do.0"
+
+  node_pool {
+    name       = "default"
+    size       = "s-2vcpu-2gb"
+    node_count = 2
+  }
+}
+
+variable "do_token" {
+  default = ""
+}
+
+variable "region" {
+  default = ""
+}
+
+output "jenkins_ip" {
+  value = digitalocean_droplet.jenkins.ipv4_adress
+}
+
+resource "local_file" "foo" {
+  content  = digitalocean_kubernetes_cluster.k8s.kube_config.0.raw_config
+  filename = "kube_config.yaml"
+}
